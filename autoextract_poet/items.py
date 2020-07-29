a4f1aed5f0ed74e09f99eb652c4287f9aa39e3a3
Victor Torres <vpaivatorres@gmail.com>
2020-07-29 17:29:46 -0300

    Add AutoExtract Article and Product items

---
+++ autoextract_poet/items.py
@@ -0,0 +1,71 @@
+from typing import Dict, List, Optional, Tuple
+
+import attr
+
+
+@attr.s(frozen=False, hash=True, auto_attribs=True, slots=True)
+class Offer:
+
+    price: Optional[str] = None
+    currency: Optional[str] = None
+    availability: Optional[str] = None
+    regularPrice: Optional[str] = None
+
+
+@attr.s(frozen=True, auto_attribs=True, slots=True)
+class Breadcrumb:
+
+    name: Optional[str] = None
+    link: Optional[str] = None
+
+
+@attr.s(frozen=True, auto_attribs=True, slots=True)
+class Rating:
+
+    ratingValue: Optional[float]
+    bestRating: Optional[float]
+    reviewCount: Optional[int]
+
+
+@attr.s(auto_attribs=True, slots=True)
+class Article:
+
+    headline: Optional[str] = None
+    datePublished: Optional[str] = None
+    datePublishedRaw: Optional[str] = None
+    dateModified: Optional[str] = None
+    dateModifiedRaw: Optional[str] = None
+    author: Optional[str] = None
+    authorsList: Optional[List[str]] = None
+    inLanguage: Optional[str] = None
+    breadcrumbs: Optional[List[Breadcrumb]] = None
+    mainImage: Optional[str] = None
+    images: Optional[List[str]] = None
+    description: Optional[str] = None
+    articleBody: Optional[str] = None
+    articleBodyHtml: Optional[str] = None
+    articleBodyRaw: Optional[str] = None
+    videoUrls: Optional[List[str]] = None
+    audioUrls: Optional[List[str]] = None
+    probability: Optional[float] = None
+    canonicalUrl: Optional[str] = None
+    url: Optional[str] = None
+
+
+@attr.s(auto_attribs=True, slots=True)
+class Product:
+
+    name: Optional[str] = None
+    offers: Optional[List[Offer]] = None
+    sku: Optional[str] = None
+    gtin: Optional[List[Dict[str, str]]] = None
+    mpn: Optional[str] = None
+    brand: Optional[str] = None
+    breadcrumbs: Optional[List[Breadcrumb]] = None
+    mainImage: Optional[str] = None
+    images: Optional[List[str]] = None
+    description: Optional[str] = None
+    probability: Optional[float] = None
+    url: Optional[str] = None
+    additionalProperty: Optional[List[Tuple[str, str]]] = None
+    aggregateRating: Optional[Rating] = None
---
+++ tests/test_items.py
@@ -0,0 +1,198 @@
+import pytest
+
+from autoextract_poet.items import (
+    Offer,
+    Breadcrumb,
+    Rating,
+    Article,
+    Product,
+)
+
+# https://doc.scrapinghub.com/autoextract/article.html
+example_article_result = {
+    "article": {
+        "headline": "Article headline",
+        "datePublished": "2019-06-19T00:00:00",
+        "datePublishedRaw": "June 19, 2019",
+        "dateModified": "2019-06-21T00:00:00",
+        "dateModifiedRaw": "June 21, 2019",
+        "author": "Article author",
+        "authorsList": [
+            "Article author"
+        ],
+        "inLanguage": "en",
+        "breadcrumbs": [
+            {
+                "name": "Level 1",
+                "link": "http://example.com"
+            }
+        ],
+        "mainImage": "http://example.com/image.png",
+        "images": [
+            "http://example.com/image.png"
+        ],
+        "description": "Article summary",
+        "articleBody": "Article body ...",
+        "articleBodyHtml": "<article><p>Article body ... </p> ... </article>",
+        "articleBodyRaw": "<div id=\"an-article\">Article body ...",
+        "videoUrls": [
+            "https://example.com/video.mp4"
+        ],
+        "audioUrls": [
+            "https://example.com/audio.mp3"
+        ],
+        "probability": 0.95,
+        "canonicalUrl": "https://example.com/article/article-about-something",
+        "url": "https://example.com/article?id=24"
+    },
+    "webPage": {
+        "inLanguages": [
+            {"code": "en"},
+            {"code": "es"}
+        ]
+    },
+    "query": {
+        "id": "1564747029122-9e02a1868d70b7a3",
+        "domain": "example.com",
+        "userQuery": {
+            "pageType": "article",
+            "url": "http://example.com/article?id=24"
+        }
+    }
+}
+
+# https://doc.scrapinghub.com/autoextract/product.html
+example_product_result = {
+    "product": {
+        "name": "Product name",
+        "offers": [
+            {
+                "price": "42",
+                "currency": "USD",
+                "availability": "InStock"
+            }
+        ],
+        "sku": "product sku",
+        "mpn": "product mpn",
+        "gtin": [
+            {
+                "type": "ean13",
+                "value": "978-3-16-148410-0"
+            }
+        ],
+        "brand": "product brand",
+        "breadcrumbs": [
+            {
+                "name": "Level 1",
+                "link": "http://example.com"
+            }
+        ],
+        "mainImage": "http://example.com/image.png",
+        "images": [
+            "http://example.com/image.png"
+        ],
+        "description": "product description",
+        "aggregateRating": {
+            "ratingValue": 4.5,
+            "bestRating": 5.0,
+            "reviewCount": 31
+        },
+        "additionalProperty": [
+            {
+                "name": "property 1",
+                "value": "value of property 1"
+            }
+        ],
+        "probability": 0.95,
+        "url": "https://example.com/product"
+    },
+    "webPage": {
+        "inLanguages": [
+            {"code": "en"},
+            {"code": "es"}
+        ]
+    },
+    "query": {
+        "id": "1564747029122-9e02a1868d70b7a2",
+        "domain": "example.com",
+        "userQuery": {
+            "pageType": "product",
+            "url": "https://example.com/product"
+        }
+    }
+}
+
+
+@pytest.mark.parametrize("offer", example_product_result["product"]["offers"])  # type: ignore
+def test_offer(offer):
+    item = Offer(**offer)
+    for key, value in offer.items():
+        assert getattr(item, key) == value
+
+    # AttributeError: 'Offer' object has no attribute 'foo'
+    with pytest.raises(AttributeError):
+        item.foo = "bar"
+
+    # TypeError: __init__() got an unexpected argument 'foo'
+    with pytest.raises(TypeError):
+        Offer(**offer, foo="bar")
+
+
+@pytest.mark.parametrize("breadcrumb", example_product_result["product"]["breadcrumbs"])  # type: ignore
+def test_breadcrumb(breadcrumb):
+    item = Breadcrumb(**breadcrumb)
+    for key, value in breadcrumb.items():
+        assert getattr(item, key) == value
+
+    # AttributeError: 'Breadcrumb' object has no attribute 'foo'
+    with pytest.raises(AttributeError):
+        item.foo = "bar"
+
+    # TypeError: __init__() got an unexpected argument 'foo'
+    with pytest.raises(TypeError):
+        Breadcrumb(**breadcrumb, foo="bar")
+
+
+def test_rating():
+    rating = example_product_result["product"]["aggregateRating"]
+    item = Rating(**rating)
+    for key, value in rating.items():
+        assert getattr(item, key) == value
+
+    # AttributeError: 'Rating' object has no attribute 'foo'
+    with pytest.raises(AttributeError):
+        item.foo = "bar"
+
+    # TypeError: __init__() got an unexpected argument 'foo'
+    with pytest.raises(TypeError):
+        Rating(**rating, foo="bar")
+
+
+def test_product():
+    product = example_product_result["product"]
+    item = Product(**product)
+    for key, value in product.items():
+        assert getattr(item, key) == value
+
+    # AttributeError: 'Product' object has no attribute 'foo'
+    with pytest.raises(AttributeError):
+        item.foo = "bar"
+
+    # TypeError: __init__() got an unexpected argument 'foo'
+    with pytest.raises(TypeError):
+        Product(**product, foo="bar")
+
+
+def test_article():
+    article = example_article_result["article"]
+    item = Article(**article)
+    for key, value in article.items():
+        assert getattr(item, key) == value
+
+    # AttributeError: 'Article' object has no attribute 'foo'
+    with pytest.raises(AttributeError):
+        item.foo = "bar"
+
+    # TypeError: __init__() got an unexpected argument 'foo'
+    with pytest.raises(TypeError):
+        Article(**article, foo="bar")
